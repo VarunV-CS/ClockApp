d361a28a635cf9729ac0707638b8d15d849ab8fc
VarunV-CS <varun.vanam@cestasoft.com>
2025-11-17 16:45:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Stopwatch from './pages/Stopwatch.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Stopwatch/>
+    <Stopwatch/>
+    <Stopwatch/>
+    </>
+  );
+
+}
+
+export default App
